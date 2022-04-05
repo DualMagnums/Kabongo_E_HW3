5b9b30e76eb4e3d0d64f673747ceb4585a397ba6
Emmanuel Kabongo <91140751+DualMagnums@users.noreply.github.com>
2022-04-04 20:25:19 -0400

    Add files via upload

---
+++ Homework Files/components/vars.py
@@ -0,0 +1,9 @@
+quizTotal = 0
+character = None
+
+characters = ["Hulk", "Loki", "Black Widow", "Iron Man"]
+
+#optional variable for if you decide to use the Pillow package to display images
+#you will need to add these images to this project
+images = ["hulk.jpg", "Loki.jpg", "natasha.jpg", "stark.jpg"]
+#I wil not be adding images :(
